6afe9e3fff468ebae3703822effdee761745d72a
artus9033 <artus9033@gmail.com>
2021-01-27 13:57:24 +0100

    Buld & publish package to PyPi will now be triggered when a commit with tag v* is pushed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name='mnultitool',
-    version='1.0.3',
+    version='1.0.4',
     description='A multitool for Numerical Methods',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -27,7 +27,6 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
     ],
     keywords='numerical,math,interpolation',
-
     packages=find_packages(),
     python_requires='>=3.6, <4',
 )
